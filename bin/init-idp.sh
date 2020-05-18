b94e87cbaab0fa7b8b0842cb10828d9e7b6756e4
Sami Silén <sasilen@users.noreply.github.com>
2020-05-18 18:22:50 +0300

    Update init-idp.sh

--- bin/init-idp.sh
+++ bin/init-idp.sh
@@ -27,5 +27,7 @@ cp -r edit-webapp/css/ /ext-mount/customized-shibboleth-idp/webapp/
 cp -r edit-webapp/images/ /ext-mount/customized-shibboleth-idp/webapp/
 rm -r /ext-mount/customized-shibboleth-idp/views/user-prefs.js
 
+chmod -R 755 /ext-mount/customized-shibboleth-idp/
+
 echo "A basic Shibboleth IdP config and UI has been copied to ./customized-shibboleth-idp/ (assuming the default volume mapping was used)."
 echo "Most files, if not being customized can be removed from what was exported/the local Docker image and baseline files will be used."
