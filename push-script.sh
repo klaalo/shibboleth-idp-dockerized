c3f95e6cabeacbc1e5fc38d31617ab270d64fd60
Kari Laalo <kari.laalo@weare.fi>
2021-07-30 19:28:08 +0300

    Use variable in Travis env

--- push-script.sh
+++ push-script.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 IDP_VERSION=$(sed -rn 's/.*idp_version=([0-9]\.[0-9]\.[0-9]).*/\1/p' Dockerfile)
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t klaalo/shibboleth-idp .
-docker push klaalo/shibboleth-idp
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker build -t ${DOCKER_USERNAME}/shibboleth-idp .
+docker push ${DOCKER_USERNAME}/shibboleth-idp
 # Push tagged version
 echo "Info: Pushing with version tag: ${IDP_VERSION}"
-docker build -t klaalo/shibboleth-idp:${IDP_VERSION} .
-docker push klaalo/shibboleth-idp:${IDP_VERSION}
+docker build -t ${DOCKER_USERNAME}/shibboleth-idp:${IDP_VERSION} .
+docker push ${DOCKER_USERNAME}/shibboleth-idp:${IDP_VERSION}
