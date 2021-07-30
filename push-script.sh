9707032914d5cfc02b1240cb0dd7eb4383ec6a92
Kari Laalo <kari.laalo@weare.fi>
2021-07-30 19:11:16 +0300

    Use separate push script to tag version number

---
+++ push-script.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+IDP_VERION=$(sed -rn 's/.*idp_version=([0-9]\.[0-9]\.[0-9]).*/\1/p' Dockerfile)
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t klaalo/shibboleth-idp .
+docker push klaalo/shibboleth-idp
+# Push tagged version
+echo "Info: Pushing with version tag: ${IDP_VERSION}"
+docker build -t klaalo/shibboleth-idp:${IDP_VERSION} .
+docker push klaalo/shibboleth-idp:${IDP_VERSION}
