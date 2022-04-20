c534e83cb68f2fbb3f23d3bfc7cf92f7ee89fd6f
Kari Laalo <kari.laalo@weare.fi>
2022-04-20 21:14:43 +0300

    Remove temporary postfix in tag

--- push-script.sh
+++ push-script.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 IDP_VERSION=$(sed -rn 's/.*idp_version=([0-9]\.[0-9]\.[0-9]).*/\1/p' Dockerfile)
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-#docker build -t ${DOCKER_USERNAME}/shibboleth-idp .
-#docker push ${DOCKER_USERNAME}/shibboleth-idp
+docker build -t ${DOCKER_USERNAME}/shibboleth-idp .
+docker push ${DOCKER_USERNAME}/shibboleth-idp
 # Push tagged version
-echo "Info: Pushing with version tag: ${IDP_VERSION}-test"
-docker build -t ${DOCKER_USERNAME}/shibboleth-idp:${IDP_VERSION}-test .
-docker push ${DOCKER_USERNAME}/shibboleth-idp:${IDP_VERSION}-test
+echo "Info: Pushing with version tag: ${IDP_VERSION}"
+docker push ${DOCKER_USERNAME}/shibboleth-idp:${IDP_VERSION}
