5f88a7ff0b0a7d49151dbc50c947c8ab4c867889
Kari Laalo <kari.laalo@weare.fi>
2022-07-12 17:33:02 +0300

    Use proper Docker context directories

--- push-script.sh
+++ push-script.sh
@@ -23,12 +23,12 @@ docker push ${DOCKER_USERNAME}/shibboleth-idp
 #####
 ### Push Jetty v10 version
 #
-IDP_VERSION=$(sed -rn 's/.*idp_version=([0-9]\.[0-9]\.[0-9]).*/\1/p' Dockerfile)
-build ${IDP_VERSION} Dockerfile
+IDP_VERSION=$(sed -rn 's/.*idp_version=([0-9]\.[0-9]\.[0-9]).*/\1/p' ./latest/Dockerfile)
+build ${IDP_VERSION} ./latest/
 
 #####
 ### Build old Jetty v9 version
 #
-IDP_VERSION=$(sed -rn 's/.*idp_version=([0-9]\.[0-9]\.[0-9]).*/\1/p' Dockerfile-jetty9-jdk11)
+IDP_VERSION=$(sed -rn 's/.*idp_version=([0-9]\.[0-9]\.[0-9]).*/\1/p' ./jetty9-jdk11/Dockerfile)
 TAG_VERSION=${IDP_VERSION}-jetty9-jdk11
-build ${TAG_VERSION} Dockerfile-jetty9-jdk11
+build ${TAG_VERSION} ./jetty9-jdk11/
